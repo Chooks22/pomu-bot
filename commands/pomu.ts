c674db29a98f9a89fcd6f44e7e8dc7cdcad0b7a9
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-08-16 12:30:15 +0800

    Update pomu command

--- commands/pomu.ts
+++ commands/pomu.ts
@@ -1,4 +1,5 @@
-import { CommandInteraction, defineCommand, FetchUtil } from 'chookscord';
+import { CommandInteraction, defineCommand } from 'chookscord';
+import fetch from 'node-fetch';
 
 interface PomuCount {
   success: boolean;
@@ -6,7 +7,21 @@ interface PomuCount {
 }
 
 const pomu = {
-  async get(fetch: FetchUtil, interaction: CommandInteraction) {
+  async get(interaction: CommandInteraction) {
+    const response = await fetch('https://impomu.com/get', {
+      method: 'POST',
+    });
+
+    if (!response.ok) {
+      interaction.reply('Failed to get Pomu count!');
+      return;
+    }
+
+    const data = await response.json() as PomuCount;
+    const pomus = Number(data.count).toLocaleString('en-GB');
+    interaction.reply(`Total Pomus: ${pomus}`);
+  },
+  async add(interaction: CommandInteraction) {
     const response = await fetch('https://impomu.com/add');
     if (response.ok) {
       interaction.reply('I\'m Pomu!');
@@ -14,35 +29,26 @@ const pomu = {
       interaction.reply('Pomu\'s not here right now');
     }
   },
-  async add(fetch: FetchUtil, interaction: CommandInteraction) {
-    const response = await fetch.get<PomuCount>('https://impomu.com/get');
-    if (!response.ok) {
-      interaction.reply('Failed to get Pomu count!');
-      return;
-    }
-    const pomus = Number(response.data.count).toLocaleString('en-GB');
-    interaction.reply(`Total Pomus: ${pomus}`);
-  },
 };
 
 export default defineCommand({
   name: 'pomu',
   description: 'Im Pomu',
-  execute({ fetch, interaction }) {
+  execute({ interaction }) {
     if (!interaction.options) return;
     const subCommand = interaction.options.getSubcommand() as keyof typeof pomu;
-    pomu[subCommand](fetch, interaction);
+    pomu[subCommand](interaction);
   },
   options: [
     {
       name: 'get',
       description: 'Get total Pomus',
-      type: 'SUB_COMMAND',
+      type: 1 as any,
     },
     {
       name: 'add',
       description: 'Add a Pomu',
-      type: 'SUB_COMMAND',
+      type: 1 as any,
     },
   ],
 });
