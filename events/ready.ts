c650f265a6fd4ee2be04a7869d3fbf584f42be61
Chooks22 <49726759+Choooks22@users.noreply.github.com>
2021-09-05 19:18:07 +0800

    Use logger in ready event

--- events/ready.ts
+++ events/ready.ts
@@ -3,7 +3,7 @@ import { defineEvent } from 'chookscord';
 export default defineEvent({
   name: 'ready',
   once: true,
-  execute() {
-    console.info('I\'m Pomu!');
+  execute({ logger }) {
+    logger.info('I\'m Pomu!');
   },
 });
